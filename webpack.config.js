4f7d36228a4a3bbd8448aea4e26ee60aa713939e
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2024-08-11 23:43:30 -0300

    chore: setup babel

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,18 @@ module.exports = {
         exclude: "/node_modules",
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: "/node_modules",
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
     ]
   }
 };
